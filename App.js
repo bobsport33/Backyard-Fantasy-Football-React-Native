b068977c58ae8649aadd3a3a8ab283fe60647611
bob <bkelly@impactnetworking.com>
2023-04-24 16:32:24 -0500

    adjusted packages for build

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 
-import { Button, StyleSheet } from "react-native";
-import ColorButton from "./components/ColorButton";
 import Colors from "./constants/colors";
 
 import Home from "./screens/Home";
-import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import Search from "./screens/Search";
 import SearchResults from "./screens/SearchResults";
@@ -26,21 +24,7 @@ export default function App() {
                     },
                 }}
             >
-                <Stack.Screen
-                    name="Home"
-                    component={Home}
-                    options={({ navigation }) => ({
-                        headerRight: () => (
-                            <ColorButton
-                                onPress={() => navigation.navigate("Login")}
-                                title="Login"
-                                textColor={Colors.blue}
-                                backgroundColor={Colors.dark}
-                            />
-                        ),
-                    })}
-                />
-                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Signup" component={Signup} />
                 <Stack.Screen name="Search" component={Search} />
                 <Stack.Screen
